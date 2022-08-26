81a4be73f2d8c8efe668f46fa0b00fd370a530c4
Abuhurairah127 <abuhurairahmuhammadshafique@gmail.com>
2022-08-26 19:39:35 +0500

    software list update 1

--- softwareList.js
+++ softwareList.js
@@ -2,5 +2,57 @@ let softwareList = [
   {
     name: "Visual Studio Code Editor",
     downloadUrl: "https://code.visualstudio.com/download",
+    usage: "Universal Code Editor",
+  },
+  {
+    name: "Git",
+    downloadUrl: "https://git-scm.com/downloads",
+    usage: "Version control system",
+  },
+  {
+    name: "Github",
+    downloadUrl: "https://desktop.github.com/",
+    usage: "GUI tool for git (Version Control System)",
+  },
+  {
+    name: "Google Chrome Browser",
+    downloadUrl: "https://www.google.com/intl/en_pk/chrome/",
+    usage: "Browser to watch web",
+  },
+  {
+    name: "Windows Terminal",
+    downloadUrl:
+      "https://apps.microsoft.com/store/detail/windows-terminal/9N0DX20HK701?hl=en-pk&gl=PK",
+    usage:
+      "A terminal with the option to open many different(types such as CMD, Windows PowerShell or Git Bash) terminals in a single window",
+  },
+  {
+    name: "Postman",
+    downloadUrl: "https://www.postman.com/downloads/",
+    usage: "A GUI tool to send API requests with out writing code",
+  },
+  {
+    name: "Figma Desktop",
+    downloadUrl: "https://www.figma.com/downloads/",
+    usage:
+      "A design tool where designer designs the web or the app and developer watch it to turn it into code",
+  },
+  {
+    name: "Adobe Xd",
+    downloadUrl: "https://helpx.adobe.com/xd/get-started.html",
+    usage:
+      "A design tool where designer designs the web or the app and developer watch it to turn it into code",
+  },
+  {
+    name: "Adobe Xd",
+    downloadUrl: "https://helpx.adobe.com/xd/get-started.html",
+    usage:
+      "A design tool where designer designs the web or the app and developer watch it to turn it into code",
+  },
+  {
+    name: "Node js",
+    downloadUrl: "https://nodejs.org/en/",
+    usage:
+      "A tool to add/download libraries to your application/project by using commands such as npm install <library-name>",
   },
 ];
