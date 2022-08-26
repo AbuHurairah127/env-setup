03b0934fb19dedacd8d2f1b89e9d7dab4861f4b4
Abuhurairah127 <abuhurairahmuhammadshafique@gmail.com>
2022-08-27 01:02:51 +0500

    Extentions list completed V1

---
+++ VSCodeextentionsList.js
@@ -0,0 +1,108 @@
+let VS_CODE_EXTENSIONS_LIST = [
+  {
+    name: "Auto Close Tag",
+    usage: "Automatically add HTML/XML close tag",
+  },
+  {
+    name: "Auto Complete Tag",
+    usage:
+      "Extension Packs to add close tag and rename paired tag automatically for HTML/XML",
+  },
+  {
+    name: "Auto Rename Tag",
+    usage: "Auto rename paired HTML/XML tag",
+  },
+  {
+    name: "Code Spell Checker",
+    usage: "Spelling checker for source code",
+  },
+  {
+    name: "CSS Peek",
+    usage:
+      "Allow peeking to css ID and class strings as definitions from html files to respective CSS. Allows peek and goto definition.",
+  },
+  {
+    name: "ES7 React/Redux/GraphQL/React-Native snippets",
+    usage:
+      "Simple extensions for React, Redux and Graphql in JS/TS with ES7 syntax (rodrigovallades).",
+  },
+  {
+    name: "ES7 React/Redux//React-Native snippets",
+    usage:
+      "Extensions for React, React-Native and Redux in JS/TS with ES7+ syntax. Customizable. Built-in integration with prettier(dsznajder)",
+  },
+  {
+    name: "File Peek",
+    usage:
+      "Allow peeking to file name strings as definitions from javascript and typescript code. Allows peek and goto definition. Great for Angular template and style files)",
+  },
+  {
+    name: "Git Lens",
+    usage:
+      "Supercharge Git within VS Code — Visualize code authorship at a glance via Git blame annotations and CodeLens, seamlessly navigate and explore Git repositories, gain valuable insights via rich visualizations and powerful comparison commands, and so much more",
+  },
+  {
+    name: "HTML CSS Support",
+    usage: "CSS Intellisense for HTML",
+  },
+  {
+    name: "Import Cost",
+    usage: "Display import/require package size in the editor",
+  },
+  {
+    name: "Indent-Rainbow",
+    usage: "Makes indentation easier to read",
+  },
+  {
+    name: "Live Server",
+    usage:
+      "Launch a development local Server with live reload feature for static & dynamic pages",
+  },
+  {
+    name: "Material Icon Theme",
+    usage: "Material Design Icons for Visual Studio Code",
+  },
+  {
+    name: "Mintlify Doc Writer ...",
+    usage:
+      "AI powered documentation writer for JavaScript, Python, Java, Typescript & all other languages",
+  },
+  {
+    name: "Prettier - Code formatter",
+    usage: "Code formatter using prettier",
+  },
+  {
+    name: "Quokka.js",
+    usage: "JavaScript and TypeScript playground in your editor.",
+  },
+  {
+    name: "RapidAPI Client",
+    usage:
+      "RapidAPI Client is a full-featured HTTP client that lets you test and describes the APIs you build or consume. Designed to work with your VS Code themes, RapidAPI Client makes composing requests, inspecting responses, generating code, and types for application development simple and intuitive.",
+  },
+  {
+    name: "React Native Tools",
+    usage: "Debugging and integrated commands for React Native",
+  },
+  {
+    name: "Tabnine AI Autocomplete Code",
+    usage:
+      "JavaScript, Python, Java, Typescript & all other languages - AI Code completion plugin. Tabnine makes developers more productive by auto-completing their code.",
+  },
+  {
+    name: "Tailwind CSS IntelliSense",
+    usage: "Intelligent Tailwind CSS tooling for VS Code",
+  },
+  {
+    name: "Thunder Client",
+    usage: "Lightweight Rest API Client for VS Code",
+  },
+  {
+    name: "TODO Highlight",
+    usage: "highlight TODOs, FIXMEs, and any keywords, annotations...",
+  },
+  {
+    name: "Turbo Console Log",
+    usage: "Automating the process of writing meaningful log messages.",
+  },
+];
