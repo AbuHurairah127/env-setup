aa74de20107c550f6248b32965c042512725fc8c
Abu Hurairah <91326530+AbuHurairah127@users.noreply.github.com>
2022-09-19 23:29:11 +0500

    grammerly added

---
+++ ChormeExtentionsList.js
@@ -0,0 +1,8 @@
+let chorme_extentions_list = [
+  {
+    name: "Grammarly",
+    usage:
+      "to check the spelling and grammer of any line or passage entering in th input field",
+    downloadLink: "",
+  },
+];
