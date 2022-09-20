51c73ed2add65220e9a797789df8ed8a55ebadbf
Abu Hurairah <91326530+AbuHurairah127@users.noreply.github.com>
2022-09-20 18:49:59 +0500

    chrome extention list updated

--- ChormeExtentionsList.js
+++ ChormeExtentionsList.js
@@ -3,6 +3,36 @@ let chorme_extentions_list = [
     name: "Grammarly",
     usage:
       "to check the spelling and grammer of any line or passage entering in th input field",
+    downloadLink: "https://www.grammarly.com/browser/chrome",
+  },
+  {
+    name: "Whatfont",
+    usage: "It is used to check the font properties used in any web page",
+    downloadLink:
+      "https://chrome.google.com/webstore/detail/whatfont/jabopobgcpjmedljpbcaablpmlmfcogm",
+  },
+  {
+    name: "React Developer Tools",
+    usage:
+      "It is used to check the data flow and components relation with each other on Chrome(browser)",
+    downloadLink:
+      "https://chrome.google.com/webstore/detail/react-developer-tools/fmkadmapgofadopljbjfkapdkoienihi?hl=en",
+  },
+  {
+    name: "Site Palette",
+    usage: "It is used to create a color palette of any website",
+    downloadLink:
+      "https://chrome.google.com/webstore/detail/site-palette/pekhihjiehdafocefoimckjpbkegknoh?hl=en",
+  },
+  {
+    name: "Color Zilla",
+    usage: "Color picker from any site give all details of color",
+    downloadLink:
+      "https://chrome.google.com/webstore/detail/colorzilla/bhlhnicpbhignbdhedgjhgdocnmhomnp?hl=en",
+  },
+  {
+    name: "Fake Filler",
+    usage: "It is use to add fake data in the forms while web testing",
     downloadLink: "",
   },
 ];
